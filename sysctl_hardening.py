6a7e8379c512947e3aedf5096ef0d5d8b82cf3ca
Blue Potato <105396427+BluePotato102@users.noreply.github.com>
2024-12-25 09:52:13 -0600

    Update sysctl_hardening.py

--- sysctl_hardening.py
+++ sysctl_hardening.py
@@ -17,7 +17,9 @@ sysctl_settings = {
     "kernel.exec-shield": 1,
     "fs.protected_symlinks": 1,
     "kernel.sysrq": 0,
-    "kernel.kptr_restrict": 2
+    "kernel.kptr_restrict": 2,
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1
+
 }
 
 # Path to sysctl.conf
