b982e6339b19164ca07346e47b24a589f6457230
Blue Potato <105396427+BluePotato102@users.noreply.github.com>
2025-01-05 11:17:00 -0600

    Update package_audit.py

--- package_audit.py
+++ package_audit.py
@@ -15,7 +15,7 @@ packages = [
     'seclists', 'pyrit', 'proxychains', 'proxychains-ng', 'vsftpd', 'samba', 'apache', 
     'nginx', 'pop3', 'smtp', 'squid', 'airmon-ng', 'hydra-gtk', 'johnny', 'hive', 
     'burp', 'cainandable', 'myheritage', 'zenmap', 'telnet', 'postfix', 'lcrack', 'sl', 'Freeciv',
-    'netdiag'
+    'netdiag', "sucrack"
 ]
 
 # Function to check if a package is installed
